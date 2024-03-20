0366ec4ffd3d68f477f8ce6c1f9aa3863f7997b9
Filip Lobpreis <f.lobpreis@gmail.com>
2024-03-20 09:09:57 +0100

    fix: robot: exit from skeleton
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- demoRMR/robot/robot.cpp
+++ demoRMR/robot/robot.cpp
@@ -259,7 +259,11 @@ void Robot::skeletonprocess()
     std::cout<<::setsockopt(ske_s, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof timeout)<<std::endl;
     std::cout<<::setsockopt(ske_s,SOL_SOCKET,SO_BROADCAST,&ske_broadcastene,sizeof(ske_broadcastene))<<std::endl;
 #else
-    ::setsockopt(ske_s,SOL_SOCKET,SO_BROADCAST,&ske_broadcastene,sizeof(ske_broadcastene));
+	struct timeval tv;
+	tv.tv_sec = 0;
+	tv.tv_usec = 500000;
+    ::setsockopt(ske_s,SOL_SOCKET,SO_BROADCAST, &ske_broadcastene,sizeof(ske_broadcastene));
+    ::setsockopt(ske_s,SOL_SOCKET,SO_RCVTIMEO, (const char *)&tv,sizeof(tv));
 #endif
     // zero out the structure
     memset((char *) &ske_si_me, 0, sizeof(ske_si_me));
